4ee8c2703aa93f74d3e81c112b2b619ada576ed9
tim <dzhoroev1@gmail.com>
2023-01-19 14:09:56 +0900

    [add] js css files added

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './components/Experience'
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+    <Canvas
+        shadows={ true }
+        camera={ {
+            fov: 45,
+            near: 0.1,
+            far: 200,
+            position: [ - 4, 3, 6 ]
+        } }
+    >
+        <Experience />
+    </Canvas>
+)
