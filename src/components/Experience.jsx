661333150aeab905699af3506976531681727d1b
tim <dzhoroev1@gmail.com>
2023-01-25 21:27:25 +0900

    [add] key event

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,22 +6,51 @@ import Fox from "./Fox"
 import Grass from "./Grass"
 import { useFrame } from "@react-three/fiber"
 
+const useKeyPress = targetKey => {
+    const [keyPressed, setKeyPressed] = useState(false);
+  
+    const downHandler = ({ key }) => {
+      if (key === targetKey) setKeyPressed(true);
+    };
+  
+    const upHandler = ({ key }) => {
+      if (key === targetKey) setKeyPressed(false);
+    };
+    
+    useEffect(() => {
+        window.addEventListener('keydown', downHandler);
+        window.addEventListener('keyup', upHandler);
+    
+        return () => {
+          window.removeEventListener('keydown', downHandler);
+          window.removeEventListener('keyup', upHandler);
+        };
+      }, []);
+
+      return keyPressed;
+}
+
 export default function Experience(){
 
-    const fox = useRef()
+    const forwardPressed = useKeyPress('w')
+    const backPressed = useKeyPress('s')
+    
+    const cubeRef = useRef()
 
     const { foxPosition } = useControls ('fox position', {
         foxPosition : {value: [0, -1, 0]}
     })
     
     
-    useFrame((state) => 
+    useFrame((state, delta) => 
     {
-        state.camera.position.x = foxPosition[0]
-        state.camera.position.y = foxPosition[1] + 4
-        state.camera.position.z = foxPosition[2] - 8
-        state.camera.lookAt(foxPosition[0], foxPosition[1], foxPosition[2])
-    })
+        // state.camera.position.x = foxPosition[0]
+        // state.camera.position.y = foxPosition[1] + 4
+        // state.camera.position.z = foxPosition[2] - 8
+        // state.camera.lookAt(foxPosition[0], foxPosition[1], foxPosition[2])
+        // cubeRef.current.rotation.y -= delta * 0.5
+
+    }, )
 
     return <>
     
@@ -36,8 +65,12 @@ export default function Experience(){
 
         <Grass />
         
-        <Fox ref={fox} foxAnimation={'Walk'}  scale={ 0.02 } position={foxPosition} foxName='하카'/> 
+        <Fox foxAnimation={'Walk'}  scale={ 0.02 } position={foxPosition} foxName='하카'/> 
 
+        <mesh ref={ cubeRef } rotation-y={ Math.PI * 0.25 } position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+        </mesh>
 
     </>
 }
