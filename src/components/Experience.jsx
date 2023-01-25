2980347f0e4d53ebcd3245b18417c93b69ebf448
tim <dzhoroev1@gmail.com>
2023-01-25 20:36:53 +0900

    [del] clean up

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,38 +1,20 @@
-import { OrbitControls, Html } from "@react-three/drei"
 import { Perf } from "r3f-perf"
 import React, { useRef, useEffect, useState } from "react"
-import * as THREE from "three"
 import { useControls } from "leva"
 
 import Fox from "./Fox"
 import Grass from "./Grass"
 import { useFrame } from "@react-three/fiber"
 
-function randPos(){
-    let arr = []
-    arr.push(Math.random())
-    arr.push(-1)
-    arr.push(Math.random())
-
-    return arr
-}
-
 export default function Experience(){
 
-    const [key, setKey] = useState('')
     const fox = useRef()
-    const foxAnimationOptions = ['Walk', 'Run', 'Survey', 'Stop']
-    
+
     const { foxPosition } = useControls ('fox position', {
         foxPosition : {value: [0, -1, 0]}
     })
     
     
-    let foxAnimation = 'Walk'
-
-    window.addEventListener("keypress", (e) => {setKey(e.code)})
-    
-
     useFrame((state) => 
     {
         state.camera.position.x = foxPosition[0]
@@ -41,26 +23,9 @@ export default function Experience(){
         state.camera.lookAt(foxPosition[0], foxPosition[1], foxPosition[2])
     })
 
-
-
-    switch (key)
-    {
-        case 'KeyW': foxAnimation = foxAnimationOptions[0] 
-        break
-        case 'KeyS': foxAnimation = foxAnimationOptions[3] 
-        break
-        case 'KeyC': foxAnimation = foxAnimationOptions[2]
-        break
-        case 'KeyE': foxAnimation = foxAnimationOptions[1]
-        break
-    }
-    console.log(foxAnimation)
-
     return <>
     
         <Perf position="top-left"/>
-
-        {/* <OrbitControls makeDefault/> */}
 
         <directionalLight 
             position={ [ 1, 2, 3 ] } 
@@ -71,7 +36,7 @@ export default function Experience(){
 
         <Grass />
         
-        <Fox ref={fox} foxAnimation={foxAnimation}  scale={ 0.02 } position={foxPosition} foxName='하카'/> 
+        <Fox ref={fox} foxAnimation={'Walk'}  scale={ 0.02 } position={foxPosition} foxName='하카'/> 
 
 
     </>
