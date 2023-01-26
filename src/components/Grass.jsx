d4e4daf2bd18a6ed85f9d2dd01dd16d1ca9fc076
tim <dzhoroev1@gmail.com>
2023-01-26 20:06:09 +0900

    [add] grass added

--- src/components/Grass.jsx
+++ src/components/Grass.jsx
@@ -1,8 +1,31 @@
+import { Sphere, useTexture, Plane } from "@react-three/drei";
+
 export default function Grass()
 {
-
-    return <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 20 }>
-        <planeGeometry />
-        <meshBasicMaterial color="yellowgreen" />
-    </mesh>
+    const [aoMap,map,displacementMap,normalMap,roughnessMap] = useTexture([
+        "Grass001_1K_AmbientOcclusion.jpg",
+        "Grass001_1K_Color.jpg",
+        "Grass001_1K_Displacement.jpg",
+        "Grass001_1K_Normal.jpg",
+        "Grass001_1K_Roughness.jpg"
+    ]);
+    return <Plane 
+        rotation-x={ - Math.PI * 0.5 }
+        position-y={ - 1 }
+        args={[40 , 40]} 
+        positionY={2}
+    >
+        <meshPhysicalMaterial
+            aoMap={aoMap}
+            map={map}
+            displacementMap={displacementMap}
+            normalMap={normalMap}
+            roughnessMap={roughnessMap}
+            displacementScale={0.2}
+        />
+    </Plane>
+    // <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 20 }>
+    //     <planeGeometry />
+    //     <meshBasicMaterial color="yellowgreen" />
+    // </mesh>
 }
